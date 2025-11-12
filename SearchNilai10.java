aba63402fd73e059146ee4470eccdb3520fc3e60
Farrell <farrellraissa4@gmail.com>
2025-11-12 12:09:22 +0700

    2.4 Modifikasi

--- SearchNilai10.java
+++ SearchNilai10.java
@@ -1,13 +1,33 @@
+import java.util.Scanner;
+
 public class SearchNilai10 {
     public static void main(String[] args) {
-        int[] nilai = {80, 70, 90, 85, 60};
-        int key = 90;
+        Scanner sc = new Scanner(System.in);
+        int[] nilai = new int[5];
+        int key;
+        int i;
 
-        for (int i = 0; i < nilai.length; i++) {
+        // Input elemen array
+        for (i = 0; i < nilai.length; i++) {
+            System.out.print("Masukkan nilai ke-" + i + " : ");
+            nilai[i] = sc.nextInt();
+        }
+
+        // Input nilai yang ingin dicari
+        System.out.print("Masukkan nilai yang ingin dicari: ");
+        key = sc.nextInt();
+
+        // Proses pencarian
+        for (i = 0; i < nilai.length; i++) {
             if (nilai[i] == key) {
-                System.out.println("Key ditemukan pada indeks ke-" + i);
+                System.out.println("Nilai " + key + " ditemukan pada indeks ke-" + i);
                 break;
             }
+        }
+
+        // Mengecek apakah data tidak ditemukan
+        if (i == nilai.length) {
+            System.out.println("Nilai yang dicari tidak ditemukan");
         }
     }
 }
